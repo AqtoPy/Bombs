6a2e5f36841f5ca01482e576c1c27b2c2ba0e29f
AqtoPy <rekikreki66@gmail.com>
2025-02-16 21:48:06 +0300

    Create Global.gd

---
+++ Global.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var current_level = 1
+var coins = 0
+var upgrades = {
+    "speed": 1,
+    "jump": 1,
+    "health": 1
+}
+
+func add_coins(amount):
+    coins += amount
+
+func buy_upgrade(upgrade_type, cost):
+    if coins >= cost:
+        coins -= cost
+        upgrades[upgrade_type] += 1
+        return true
+    return false
