371da569f50e56fb3506a4b1ec9915d717a001ad
AqtoPy <rekikreki66@gmail.com>
2025-02-17 18:58:27 +0300

    Update DestructionSystem.gd

--- DestructionSystem.gd
+++ DestructionSystem.gd
@@ -1,17 +1,21 @@
 extends Node
 
 @export var ground_mesh: MeshInstance3D
-@export var crater_depth = 2.0
+@export var crater_depth: float = 2.0
 @export var decal_scene: PackedScene
 
 var original_mesh: ArrayMesh
 
 func _ready():
-    original_mesh = ground_mesh.mesh.duplicate()
+    # Преобразуем меш земли в ArrayMesh
+    original_mesh = ArrayMesh.new()
+    original_mesh.add_surface_from_arrays(
+        Mesh.PRIMITIVE_TRIANGLES,
+        ground_mesh.mesh.get_mesh_arrays()
+    )
     ground_mesh.mesh = original_mesh
 
 func create_crater(position: Vector3, radius: float):
-    # Деформация меша
     var mdt = MeshDataTool.new()
     mdt.create_from_surface(original_mesh, 0)
     
@@ -27,7 +31,7 @@ func create_crater(position: Vector3, radius: float):
     mdt.commit_to_surface(original_mesh)
     ground_mesh.mesh = original_mesh
     
-    # Добавление декали
+    # Добавляем декаль
     var decal = decal_scene.instantiate()
     decal.position = position
     decal.scale = Vector3(radius, 1, radius)
