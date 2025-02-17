7ec2c188973609a19950dd1abfc62531459551fa
AqtoPy <rekikreki66@gmail.com>
2025-02-17 19:18:03 +0300

    Update Global.gd

--- Global.gd
+++ Global.gd
@@ -17,3 +17,14 @@ func buy_upgrade(upgrade_type, cost):
         upgrades[upgrade_type] += 1
         return true
     return false
+
+func reset_progress():
+    # Сброс прогресса
+    current_level = 1
+    coins = 0
+    upgrades = {
+        "speed": 1,
+        "jump": 1,
+        "health": 1
+    }
+    print("Прогресс сброшен!")
